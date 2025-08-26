e3b63bc8e72b8549b7e61ee9a003d5a94979054b
abhi007-ab <228505534+abhi007-ab@users.noreply.github.com>
2025-08-26 08:00:09 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";   // treat all JS code as newer version //
+
+// alert( 3 + 3) // WE are using nodejs, not browser
+
+console.log(3
+     +
+ 3);  // code readability should be high 
+
+
+console.log("AB");
+
+
+let name  = "AB"
+let age = 20
+let isLoggedIn = false
+
+
+/* Primitive DataTypes
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => value is not assigned
+// symbol => unique 
+*/
+
+//object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
