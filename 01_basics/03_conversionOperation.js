29a622f9057d4bba999acbe5ccd4029994db5287
abhi007-ab <228505534+abhi007-ab@users.noreply.github.com>
2025-08-27 06:50:41 +0000

    Conver.&Ope

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,8 +4,8 @@ let score = "AB"
 //console.log(typeof(score));
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "AB"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
@@ -25,5 +25,68 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// *************************** Operations  *************************** //
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " abhishek"
+
+let str3 = (str1 + str2)
+// console.log(str3);
+
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 3) * 5 % 3); 
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+// ****** Excercise for ++ ******
+
+// Postfix...
+// let x = 3;
+// const y = x++;
+// console.log(`x:${x}, y:${y}`);
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// console.log(`x:${x2}, y:${y2}`);
+// // x2 is 4n; y2 is 3n
+
+// Prefix...
+// let x = 3;
+// const y = ++x;
+// console.log(`x:${x}, y:${y}`);
+// // x is 4; y is 4
+
+// let x2 = 3n;
+// const y2 = ++x2;
+// console.log(`x:${x2}, y:${y2}`);
+// // x2 is 4n; y2 is 4n
