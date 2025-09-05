3d7ccef4faa9c2b46c0ecfa82dc0b426f0b1c13d
abhi007-ab <228505534+abhi007-ab@users.noreply.github.com>
2025-09-05 06:12:19 +0000

    objects part 2

---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,57 @@
+// for singleton
+
+const tinderUser = new Object()
+
+tinderUser.id = "123abc"
+tinderUser.name = "abhi"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));   // [ 'id', 'name', 'isLoggedIn' ] -> Array
+// console.log(Object.values(tinderUser)); // [ '123abc', 'abhi', false ] -> Array
+// console.log(Object.entries(tinderUser));   // [ [ 'id', '123abc' ], [ 'name', 'abhi' ], [ 'isLoggedIn', false ] ]
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // true
+
+
+
+const regularUser = {
+    email: "fake@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "abhishek",
+            lastname: "bhalerao"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}   // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+// const obj3 = Object.assign({}, obj1, obj2)  // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    }
+]
+
+// console.log(users[1].email);  // abc@gmail.com
