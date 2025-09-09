c1c769435c02beacb50fd2ac55679fba158afe7b
abhi007-ab <228505534+abhi007-ab@users.noreply.github.com>
2025-09-09 12:14:37 +0000

    chaining methods

---
+++ 05_iterations/seven.js
@@ -0,0 +1,18 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.map( (num) => num + 10)
+// console.log(newNums);
+
+// const newNums = myNums.forEach( (item) => {
+//     console.log(item + 10);
+// })
+
+
+//Chaining
+
+// const newNums = myNums
+//                 .map( (num) => num * 10)          // [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]       
+//                 .map( (num) => num + 1)           // [11, 21, 31, 41, 51, 61, 71, 81, 91, 101]
+//                 .filter( (num) => num >= 40)      // [41, 51, 61, 71, 81, 91, 101]
+
+// console.log(newNums);
